7f964893c7f4b1fe63f98a587d4f75dd40b3e8f0
Selyss <99344963+Selyss@users.noreply.github.com>
2024-12-29 18:42:03 -0500

    update

---
+++ src/main/java/selyss/autoignite/mixin/AutoIgniteMixin.java
@@ -0,0 +1,18 @@
+package selyss.autoignite.mixin;
+
+import net.minecraft.block.TntBlock;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(TntBlock.class)
+public class AutoIgniteMixin {
+	@Redirect(method = "onBlockAdded", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;isReceivingRedstonePower(Lnet/minecraft/util/math/BlockPos;)Z"))
+	private boolean igniteTnt(World instance, BlockPos blockPos)
+	{
+		return true;
+	}
+}
+
