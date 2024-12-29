3451ec6bc2e6e0e995b14c7b5fec98f453ade6af
Selyss <99344963+Selyss@users.noreply.github.com>
2024-12-29 13:06:19 -0500

    auto ignite tnt working

--- src/main/java/selyss/autoignite/AutoIgnite.java
+++ src/main/java/selyss/autoignite/AutoIgnite.java
@@ -2,23 +2,39 @@ package selyss.autoignite;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.TntEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AutoIgnite implements ModInitializer {
 	public static final String MOD_ID = "auto-ignite";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+		// Detect TNT block placement
+		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
+			if (!world.isClient()) {
+				if (player.getStackInHand(hand).getItem() != Blocks.TNT.asItem()) {
+					return ActionResult.PASS;
+				}
+				BlockPos pos = hitResult.getBlockPos();
+				player.sendMessage(Text.of("X: " + pos.getX() + " Y: " + pos.getY() + " Z: " + pos.getZ()), false);
 
-		LOGGER.info("Hello Fabric world!");
+				ServerWorld serverWorld = (ServerWorld) world;
+				TntEntity tntEntity = new TntEntity(world, pos.getX() + 0.5, pos.getY() + 1, pos.getZ() + 0.5, player);
+				serverWorld.spawnEntity(tntEntity);
+				tntEntity.setFuse(20); // 1 second
+				player.sendMessage(Text.of("Auto-igniting TNT"), false);
+			}
+			return ActionResult.PASS;
+		});
 	}
 }
--- src/main/java/selyss/autoignite/mixin/ExampleMixin.java
+++ src/main/java/selyss/autoignite/mixin/ExampleMixin.java
@@ -1,15 +1,41 @@
 package selyss.autoignite.mixin;
 
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.TntBlock;
+import net.minecraft.entity.TntEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.*;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-@Mixin(MinecraftServer.class)
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+@Mixin(BlockItem.class)
 public class ExampleMixin {
-	@Inject(at = @At("HEAD"), method = "loadWorld")
-	private void init(CallbackInfo info) {
-		// This code is injected into the start of MinecraftServer.loadWorld()V
+	@Inject(at = @At("HEAD"), method = "useOnBlock")
+	private void onUseOnBlock(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
+		World world = context.getWorld();
+		BlockPos pos = context.getBlockPos();
+		PlayerEntity user = context.getPlayer();
+		Hand hand = context.getHand();
+//
+//		if (!world.isClient) {
+//			ServerWorld serverWorld = (ServerWorld) world;
+//			if (user.getStackInHand(hand).getItem() == Items.TNT) {
+//				ItemStack itemStack = user.getStackInHand(hand);
+//
+//				TntEntity tntEntity = new TntEntity(world, pos.getX(), pos.getY(), pos.getZ(), user);
+//				serverWorld.spawnEntity(tntEntity);
+//				tntEntity.setFuse(20); // 1 second
+//				itemStack.decrementUnlessCreative(1, user);
+//			}
+//		}
 	}
 }
+
