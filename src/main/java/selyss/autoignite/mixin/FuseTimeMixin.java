4cbcbe5125c120542d78c1d7f9058978c386604b
Selyss <99344963+Selyss@users.noreply.github.com>
2024-12-30 16:12:17 -0500

    trying to fix error

--- src/main/java/selyss/autoignite/mixin/FuseTimeMixin.java
+++ src/main/java/selyss/autoignite/mixin/FuseTimeMixin.java
@@ -3,15 +3,20 @@ package selyss.autoignite.mixin;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.TntEntity;
+import net.minecraft.entity.data.TrackedData;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TntEntity.class)
-public class FuseTimeMixin {
+public abstract class FuseTimeMixin {
+    @Shadow
+    private static TrackedData<Integer> FUSE;
+
 
     @Inject(at = @At("TAIL"), method = "<init>*")
     private void onTntEntityCreate(World world, double x, double y, double z, @Nullable LivingEntity igniter, CallbackInfo ci) {
@@ -20,7 +25,7 @@ public class FuseTimeMixin {
             return;
         }
         TntEntity tntEntity = (TntEntity) (Object) this;
-
-        tntEntity.setFuse(20); // 1 second
+        tntEntity.getDataTracker().set(FUSE, 20);
+//        tntEntity.setFuse(20); // 1 second
     }
 }
